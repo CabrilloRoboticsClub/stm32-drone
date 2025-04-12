3674278beef30163368e2aa94d80611646daeb77
Teo Schnell <schnellteo@hotmail.com>
2025-04-12 10:07:11 -0700

    PID SeaSaw

--- Example Arduino/SerialPIDTest/SerialPIDTest.ino
+++ Example Arduino/SerialPIDTest/SerialPIDTest.ino
@@ -1,158 +1,81 @@
 #include "ArduPID.h"
-
-ArduPID YAWController;
-ArduPID ROLLController;
-ArduPID PITCHController;
-
-double YAW_P = 50.0;  // Proportional Gain
-double YAW_I = 0.00;  // Integral Gain
-double YAW_D = 50.0;   // Derivative Gain
-
-double ROLL_P = 50.0;   // Derivative Gain
-double ROLL_I = 0.00;   // Derivative Gain
-double ROLL_D = 50.0;   // Derivative Gain
-
-double PITCH_P = 50.0;      // Proportional Gain
-double PITCH_I = 0.00;      // Integral Gain
-double PITCH_D = 50.0;       // Derivative Gain
-
-double YAWoutput;
-double ROLLoutput;
-double PITCHoutput;
-
-
 #include <Arduino.h>
 #include <Adafruit_BNO08x.h>
+#include <Servo.h>
 
-// For SPI mode, we need a CS pin
-#define BNO08X_CS 10
-#define BNO08X_INT 9
-#define BNO08X_RESET -1
+// ========== PID Setup ==========
+ArduPID PITCHController;
 
-// Euler orientation stored
+double PITCH_P = 10.0;
+double PITCH_I = 0.00;
+double PITCH_D = 0.0;
+
+double PITCHoutput;
+
+// ========== Orientation Data ==========
 struct euler_t {
-  double yaw;    // YAW    + Angle is LEFT ROTATION <-- We will mostly ignore this for now
-  double pitch;  // PITCH  + Angle is FORWARD ROTATION
-  double roll;   // ROLL:  + Angle is RIGHT ROTATION
-
+  double yaw;
+  double pitch;
+  double roll;
 } ypr;
 
-// Target states orientation stored
 struct target_euler_t {
-  int throttle = 0.0;
-  double yaw = 0.0;  // Note this one is in (rads/s), not an abs location (+ Rotation is LEFT)
+  int throttle = 0;
   double pitch = 0.0;
-  double roll = 0.0;
 } target_states;
 
-double yaw_rotation = 0.0;
-
-
-Adafruit_BNO08x bno08x(BNO08X_RESET);
+Adafruit_BNO08x bno08x(-1);
 sh2_SensorValue_t sensorValue;
 
-#ifdef FAST_MODE
-// Top frequency is reported to be 1000Hz (but freq is somewhat variable)
-sh2_SensorId_t reportType = SH2_GYRO_INTEGRATED_RV;
-long reportIntervalUs = 2000;
-#else
-// Top frequency is about 250Hz but this report is more accurate
 sh2_SensorId_t reportType = SH2_ARVR_STABILIZED_RV;
 long reportIntervalUs = 5000;
-#endif
+
 void setReports(sh2_SensorId_t reportType, long report_interval) {
   Serial.println("Setting desired reports");
   if (!bno08x.enableReport(reportType, report_interval)) {
-    Serial.println("Could not enable stabilized remote vector");
+    Serial.println("Could not enable report");
   }
 }
 
+// ========== ESC + Motor Setup ==========
+Servo esc1, esc2;
 
-#include <Servo.h>
+#define MOTOR_FRONT 5
+#define MOTOR_BACK 6
 
-Servo esc1, esc2, esc3, esc4;
+double T1; // Back
+double T2; // Front
 
-// Defining Motor's pin association
-#define MOTOR1 5   // Back Right
-#define MOTOR2 6   // Front Right
-#define MOTOR3 9   // Back Left
-#define MOTOR4 10  // Front Left
-
-double T1;
-double T2;
-double T3;
-double T4;
-
-double Unmapped_T1 = 0;
-double Unmapped_T2 = 0;
-double Unmapped_T3 = 0;
-double Unmapped_T4 = 0;
-
-
-
-// Defining Motor's spin direction. (This is relevent for YAW control)
-// 1 Means motors are rotating inwards (props in), -1 means rotating out (props out)
-#define MOTOR_DIRECTION 1
-
-double throttle;
-
+// ========== Setup ==========
 void setup() {
-  // ESC SETUP (Note: ESC Calibration must be done before hand, see MotorDrive.ino)
-  delay(1000);  // Wait for ESC to power up
+  delay(1000);
 
-  // Attach ESCs to their respective pins
-  esc1.attach(MOTOR1);
-  esc2.attach(MOTOR2);
-  esc3.attach(MOTOR3);
-  esc4.attach(MOTOR4);
+  esc1.attach(MOTOR_BACK);
+  esc2.attach(MOTOR_FRONT);
 
-  // Send min throttle
   esc1.writeMicroseconds(1000);
   esc2.writeMicroseconds(1000);
-  esc3.writeMicroseconds(1000);
-  esc4.writeMicroseconds(1000);
-  delay(3000);  // Wait for ESC to register min throttle
+  delay(3000);
 
-  // Open Serial Port (For Testing Only)
   Serial.begin(9600);
-  Serial.println("ESC initialized.");
+  Serial.println("2-Motor Pitch Control Init");
 
-  // IMU SETUP
-  Serial.println("Adafruit BNO08x test!");
-
-  // Try to initialize!
   if (!bno08x.begin_I2C()) {
-    //if (!bno08x.begin_UART(&Serial1)) {  // Requires a device with > 300 byte UART buffer!
-    //if (!bno08x.begin_SPI(BNO08X_CS, BNO08X_INT)) {
     Serial.println("Failed to find BNO08x chip");
     while (1) { delay(10); }
   }
   Serial.println("BNO08x Found!");
-
-
   setReports(reportType, reportIntervalUs);
-  if (!bno08x.enableReport(SH2_GYRO_INTEGRATED_RV)) {
-    Serial.println("Could not enable gyroscope report");
-    while (1)
-      ;
-  }
-
 
   Serial.println("Reading events");
   delay(100);
 
-  YAWController.begin(&yaw_rotation, &YAWoutput, &target_states.yaw, YAW_P, YAW_I, YAW_D);
-  YAWController.setOutputLimits(-200, 200);
-
-  PITCHController.begin(&ypr.pitch, &PITCHoutput, &target_states.pitch,PITCH_P, PITCH_I, PITCH_D);
+  PITCHController.begin(&ypr.pitch, &PITCHoutput, &target_states.pitch, PITCH_P, PITCH_I, PITCH_D);
   PITCHController.setOutputLimits(-200.0, 200.0);
-
-  ROLLController.begin(&ypr.roll, &ROLLoutput, &target_states.roll, ROLL_P, ROLL_I, ROLL_D);
-  ROLLController.setOutputLimits(-200.0, 200.0);
 }
-// Functions for IMU Computation
-void quaternionToEuler(double qr, double qi, double qj, double qk, euler_t* ypr, bool degrees = false) {
 
+// ========== Quaternion to Euler ==========
+void quaternionToEuler(double qr, double qi, double qj, double qk, euler_t* ypr, bool degrees = false) {
   double sqr = sq(qr);
   double sqi = sq(qi);
   double sqj = sq(qj);
@@ -169,167 +92,75 @@ void quaternionToEuler(double qr, double qi, double qj, double qk, euler_t* ypr,
   }
 }
 
-void quaternionToEulerRV(sh2_RotationVectorWAcc_t* rotational_vector, euler_t* ypr, bool degrees = false) {
-  quaternionToEuler(rotational_vector->real, rotational_vector->i, rotational_vector->j, rotational_vector->k, ypr, degrees);
+void quaternionToEulerRV(sh2_RotationVectorWAcc_t* rv, euler_t* ypr, bool degrees = false) {
+  quaternionToEuler(rv->real, rv->i, rv->j, rv->k, ypr, degrees);
 }
 
-void quaternionToEulerGI(sh2_GyroIntegratedRV_t* rotational_vector, euler_t* ypr, bool degrees = false) {
-  quaternionToEuler(rotational_vector->real, rotational_vector->i, rotational_vector->j, rotational_vector->k, ypr, degrees);
-}
-
-double mapDouble(double x, double in_min, double in_max, double out_min, double out_max) {
-  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
-}
-
-
+// ========== Main Loop ==========
 void loop() {
-
-  // Thorttle
+  // Serial input format: throttle,pitch
   if (Serial.available()) {
-    String input = Serial.readStringUntil('\n');  // Read full line
-    input.trim();                                 // Remove whitespace/newlines
+    String input = Serial.readStringUntil('\n');
+    input.trim();
 
-    // Split input into values
-    double values[4];  // Array for throttle, yaw, pitch, roll
+    double values[2];
     int index = 0;
-    char* ptr = strtok((char*)input.c_str(), ",");  // Tokenize string
+    char* ptr = strtok((char*)input.c_str(), ",");
 
-    while (ptr != NULL && index < 4) {
-      values[index] = atof(ptr);  // Convert to double
+    while (ptr != NULL && index < 2) {
+      values[index++] = atof(ptr);
       ptr = strtok(NULL, ",");
-      index++;
     }
 
-    // Ensure we received exactly 4 values
-    if (index == 4) {
+    if (index == 2) {
       target_states.throttle = values[0];
-      target_states.yaw = values[1];
-      target_states.pitch = values[2];
-      target_states.roll = values[3];
+      target_states.pitch = values[1];
 
-      // Validate throttle range
       if (target_states.throttle <= 1000 || target_states.throttle > 2000) {
         Serial.println("Invalid throttle! Enter a number between 1000 and 2000.");
       } else {
         Serial.print("Throttle: ");
         Serial.print(target_states.throttle);
-        Serial.print(", Yaw: ");
-        Serial.print(target_states.yaw);
         Serial.print(", Pitch: ");
-        Serial.print(target_states.pitch);
-        Serial.print(", Roll: ");
-        Serial.println(target_states.roll);
+        Serial.println(target_states.pitch);
       }
     } else {
-      Serial.println("Invalid input! Format: throttle,yaw,pitch,roll");
+      Serial.println("Invalid input! Format: throttle,pitch");
     }
   }
 
-
-  // IMU
+  // Read IMU data
   if (bno08x.wasReset()) {
-    Serial.print("sensor was reset ");
     setReports(reportType, reportIntervalUs);
   }
 
   if (bno08x.getSensorEvent(&sensorValue)) {
-    // in this demo only one report type will be received depending on FAST_MODE define (above)
-    switch (sensorValue.sensorId) {
-      case SH2_ARVR_STABILIZED_RV:
-        quaternionToEulerRV(&sensorValue.un.arvrStabilizedRV, &ypr, true);
-
-      case SH2_GYRO_INTEGRATED_RV:
-        // float x = sensorValue.un.gyroIntegratedRV.angVelX;
-        // float y = sensorValue.un.gyroIntegratedRV.angVelY;
-        yaw_rotation = sensorValue.un.gyroIntegratedRV.angVelZ;
-
-        // Serial.print("Angular Velocity (rad/s) - X: ");
-        // Serial.print(x, 5);
-        // Serial.print(", Y: ");
-        // Serial.print(y, 5);
-        Serial.print("Yaw Velocity (rad/s) - Z: ");
-        Serial.print(yaw_rotation, 5);  // use println to add a newline
-
-        quaternionToEulerGI(&sensorValue.un.gyroIntegratedRV, &ypr, true);
-        break;
+    if (sensorValue.sensorId == SH2_ARVR_STABILIZED_RV) {
+      quaternionToEulerRV(&sensorValue.un.arvrStabilizedRV, &ypr, true);
     }
 
     if (target_states.throttle != 0) {
-      YAWController.compute();
       PITCHController.compute();
-      ROLLController.compute();
     }
-    // For Debugging!
-    static long last = 0;
-    long now = micros();
-    Serial.print(now - last);
-    Serial.print("\t");
-    last = now;
-    Serial.print(sensorValue.status);
-    Serial.print("\t");  // This is accuracy in the range of 0 to 3
-    Serial.print(ypr.yaw);
-    Serial.print("\t");
+
+    // Debug Print
+    Serial.print("Pitch: ");
     Serial.print(ypr.pitch);
-    Serial.print("\t");  // There is some wierd stuff with Pitch here
-    Serial.print(ypr.roll);
-    Serial.print("\t|\t");
-
-    Serial.print(target_states.throttle);
-    Serial.print("\t");
-    Serial.print(target_states.yaw);
-    Serial.print("\t");
+    Serial.print("\tTarget: ");
     Serial.print(target_states.pitch);
-    Serial.print("\t");  // There is some wierd stuff with Pitch here
-    Serial.print(target_states.roll);
-    Serial.print("\t|\t");
-    Serial.print(YAWoutput);
-    Serial.print("\t");
-    Serial.print(PITCHoutput);
-    Serial.print("\t");
-    Serial.print(ROLLoutput);
-    Serial.print("\t|\t");
+    Serial.print("\tOutput: ");
+    Serial.println(PITCHoutput);
 
-    // Motor Powers!
-    //1 Back Right
-    T1 = (target_states.throttle + PITCHoutput - ROLLoutput - YAWoutput);
+    // Calculate Motor Outputs
+    T1 = target_states.throttle + PITCHoutput; // Back
+    T2 = target_states.throttle - PITCHoutput; // Front
 
-    //2 Front Right
-    T2 = (target_states.throttle - PITCHoutput - ROLLoutput + YAWoutput);
+    // Clamp to ESC range
+    T1 = constrain(T1, 1000, 2000);
+    T2 = constrain(T2, 1000, 2000);
 
-    //3 Back Left
-    T3 = (target_states.throttle + PITCHoutput + ROLLoutput - YAWoutput);
-
-    //4 Front Left
-    T4 = (target_states.throttle - PITCHoutput + ROLLoutput + YAWoutput);
-
-
-    if (target_states.throttle != 1000) {
-
-      T1 = constrain(T1, 1025, 2000);
-      T2 = constrain(T2, 1025, 2000);
-      T3 = constrain(T3, 1025, 2000);
-      T4 = constrain(T4, 1025, 2000);
-
-    } else {
-
-      T1 = 1000;
-      T2 = 1000;
-      T3 = 1000;
-      T4 = 1000;
-
-    }
-
-    esc1.writeMicroseconds(T1);
-    esc2.writeMicroseconds(T2);
-    esc3.writeMicroseconds(T3);
-    esc4.writeMicroseconds(T4);
-
-    Serial.print(T1);
-    Serial.print("\t");
-    Serial.print(T2);
-    Serial.print("\t");
-    Serial.print(T3);
-    Serial.print("\t");
-    Serial.println(T4);
+    esc1.writeMicroseconds((int)T1);
+    esc2.writeMicroseconds((int)T2);
   }
 }
+
---
+++ PythonConfigurator/PID_graph.py
@@ -0,0 +1,120 @@
+import serial
+import threading
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import matplotlib.widgets as widgets
+import time
+
+# Serial setup
+ser = serial.Serial('COM3', 9600, timeout=1)  # Adjust as needed
+
+Output = []
+Pitch = []
+Target = []
+data_lock = threading.Lock()
+
+MAX_POINTS = 5000
+show_output = True  # Flag to control the visibility of the Output graph
+
+def read_serial():
+    while True:
+        if ser.in_waiting > 0:
+            line = ser.readline().decode('utf-8').strip().split()
+            try:
+                with data_lock:
+                    for data in line:
+                        if ':' in data:
+                            Label, num = data.split(':')
+                            num = float(num)
+                            if Label == 'Output':
+                                Output.append(num)
+                            elif Label == 'Pitch':
+                                Pitch.append(num)
+                            elif Label == 'Target':
+                                Target.append(num)
+
+                            # Limit the data to MAX_POINTS
+                            if len(Output) > MAX_POINTS:
+                                Output.pop(0)
+                            if len(Pitch) > MAX_POINTS:
+                                Pitch.pop(0)
+                            if len(Target) > MAX_POINTS:
+                                Target.pop(0)
+            except Exception as error:
+                print('\nReceived:', line)
+
+def send_serial_data():
+    while True:
+        user_input = input("Enter data to send (e.g., 'Output', 'Pitch', 'Target'): ")
+        try:
+            ser.write(user_input.encode())
+            print(f"Sent: {user_input}")
+        except Exception as e:
+            print("Error sending data:", e)
+
+# Start serial threads
+serial_thread = threading.Thread(target=read_serial, daemon=True)
+send_thread = threading.Thread(target=send_serial_data, daemon=True)
+
+serial_thread.start()
+send_thread.start()
+
+# Plotting setup
+fig, ax = plt.subplots(figsize=(16, 12)) 
+line_output, = ax.plot([], [], label='Output')
+line_pitch, = ax.plot([], [], label='Pitch')
+line_target, = ax.plot([], [], label='Target')
+
+ax.set_title("Live Serial Data")
+ax.set_xlabel("Sample Index")
+ax.set_ylabel("Value")
+ax.legend(loc='upper left')
+
+# Button to toggle Output data
+def toggle_output(event):
+    global show_output
+    show_output = not show_output
+    if show_output:
+        print("Output graph enabled.")
+    else:
+        print("Output graph disabled.")
+
+# Create a button to toggle Output visibility
+ax_button = plt.axes([0.81, 0.01, 0.18, 0.05])  # Button position [x, y, width, height]
+button = widgets.Button(ax_button, 'Toggle Output')
+button.on_clicked(toggle_output)
+
+# Plot update function
+def update(frame):
+    with data_lock:
+        max_len = max(len(Output), len(Pitch), len(Target), 1)
+
+        # Update the Output graph only if show_output is True
+        if show_output:
+            line_output.set_data(range(len(Output)), Output)
+        else:
+            line_output.set_data([], [])  # Disable the Output graph by setting empty data
+
+        line_pitch.set_data(range(len(Pitch)), Pitch)
+        line_target.set_data(range(len(Target)), Target)
+
+        ax.set_xlim(max(0, max_len - MAX_POINTS), max_len)
+
+        all_data = Output + Pitch + Target
+        if all_data:
+            min_y = min(all_data)
+            max_y = max(all_data)
+            ax.set_ylim(min_y - 10, max_y + 10)
+
+    return line_output, line_pitch, line_target
+
+# Animation loop
+ani = animation.FuncAnimation(fig, update, interval=100)
+
+try:
+    plt.show()
+except KeyboardInterrupt:
+    print("Exiting...")
+
+finally:
+    ser.close()
